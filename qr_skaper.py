421af0586589b1f8f1c2ee22c058ab1110c1df3c
VincentKleis <vincent.s.kleis@gmail.com>
2026-03-06 14:15:59 +0100

    bug fix, and moved qr codes to seperate folder

--- qr_skaper.py
+++ qr_skaper.py
@@ -1,27 +1,35 @@
 import qrcode
 from PIL import Image
-data = 'https://vincentkleis.github.io/Bring-linksamlinger/'
 
 logo = Image.open('5f44a660-27d2-4221-b0fa-0479e3a0ae8f (1).png')
-
-save_path = 'qr_code.png'
-
-qr = qrcode.QRCode(
-    error_correction= qrcode.ERROR_CORRECT_H
-)
-qr.add_data(data)
-qr.make(fit=True)
-
-img = qr.make_image(fill_color="black", bakc_color="white").convert('RGB')
-
-qr_w, qr_h = img.size
 
 logo_w, logo_h = logo.width//5, logo.height//5
 
 logo = logo.resize((logo_w, logo_h), Image.Resampling.LANCZOS)
 
-position = ((qr_w - logo_w) // 2, (qr_h - logo_h) // 2)
+destinations = ["Index", "Drammen", "StokkeSkienLarvik"]
 
-img.paste(logo, position, logo)
 
-img.save(save_path)
+for dest in destinations:
+    if dest == "Index":
+        save_path = "Index_qr_code.png"
+        data = "https://vincentkleis.github.io/Bring-linksamlinger/"
+    else:
+        save_path = dest + "_qr_code.png"
+        data = "https://vincentkleis.github.io/Bring-linksamlinger/Avdelinger/" + dest
+    
+    qr = qrcode.QRCode(
+        error_correction= qrcode.ERROR_CORRECT_H
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", bakc_color="white").convert('RGB')
+
+    qr_w, qr_h = img.size
+
+    position = ((qr_w - logo_w) // 2, (qr_h - logo_h) // 2)
+
+    img.paste(logo, position, logo)
+
+    img.save("QR_coder/"+save_path)
